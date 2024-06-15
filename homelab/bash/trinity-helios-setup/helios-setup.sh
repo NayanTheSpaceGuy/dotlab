ded575e7e8eb39c8e63624f93473bd2efc9cb8d1
NayanTheSpaceGuy <147062173+NayanTheSpaceGuy@users.noreply.github.com>
2024-06-15 21:29:08 +0530

    fix: ansible locale

--- homelab/bash/trinity-helios-setup/helios-setup.sh
+++ homelab/bash/trinity-helios-setup/helios-setup.sh
@@ -1,39 +1,61 @@
 #!/bin/bash
 
+####################
+# Special Functions
+###################
 # Detect Linux distribution
-local dtype="unknown"  # Default to unknown
-# Use /etc/os-release for modern distro identification
-if [ -r /etc/os-release ]; then
-	source /etc/os-release
-	case $ID in
-		fedora|rhel|centos)
-			dtype="redhat"
-			;;
-		sles|opensuse*)
-			dtype="suse"
-			;;
-		ubuntu|debian)
-			dtype="debian"
-			;;
-		gentoo)
-			dtype="gentoo"
-			;;
-		arch)
-			dtype="arch"
-			;;
-		slackware)
-			dtype="slackware"
-			;;
-		*)
-			# If ID is not recognized, keep dtype as unknown
-			;;
-	esac
-fi
+distribution ()
+{
+    local dtype="unknown"  # Default to unknown
+    # Use /etc/os-release for modern distro identification
+    if [ -r /etc/os-release ]; then
+	    source /etc/os-release
+        case $ID in
+            fedora|rhel|centos)
+                dtype="redhat"
+                ;;
+            sles|opensuse*)
+                dtype="suse"
+                ;;
+            ubuntu|debian)
+                dtype="debian"
+                ;;
+            gentoo)
+                dtype="gentoo"
+                ;;
+            arch)
+                dtype="arch"
+                ;;
+            slackware)
+                dtype="slackware"
+                ;;
+            *)
+                # If ID is not recognized, keep dtype as unknown
+                ;;
+        esac
+    fi
 
-echo $dtype
+    echo "$dtype"
+}
+
+# Detect locale
+locale ()
+{
+    local localetype="unknown" # Default to unknown
+    if [ "$LANG" == "*.*8" ]; then
+        localetype="UTF-8"
+    else
+        localetype="UTF"
+        echo "$localetype"
+    fi
+}
+
 
 if [ "$dtype" == "debian" ]; then
-    echo "Here we go.."
+    echo "-------------"
+    echo "Here we go..."
+    echo "-------------"
+    echo ""
     echo "Detected Debian distribution. Proceeding with the setup..."
 
     echo "Updating package lists and dist-upgrading packages..."
@@ -51,7 +73,16 @@ if [ "$dtype" == "debian" ]; then
     git clone git@github.com:NayanTheSpaceGuy/dotfiles-and-homelab.git
 
     echo "Running the helios-setup ansible playbook..."
-    ansible-playbook -i ~/helios-setup/dotfiles-and-homelab/homelab/ansible/inventory/spacehlship.yml ~/helios-setup/dotfiles-and-homelab/homelab/ansible/playbooks/setup-proxmoxve/trinity-helios/helios-setup.yml
+    if [ "$localetype" == "UTF-8" ]; then
+    ansible-playbook \
+    -i ~/helios-setup/dotfiles-and-homelab/homelab/ansible/inventory/spacehlship.yml \
+    ~/helios-setup/dotfiles-and-homelab/homelab/ansible/playbooks/setup-proxmoxve/trinity-helios/helios-setup.yml
+
+    else
+    LANG=en_IN.UTF_8 ansible-playbook \
+    -i ~/helios-setup/dotfiles-and-homelab/homelab/ansible/inventory/spacehlship.yml \
+    ~/helios-setup/dotfiles-and-homelab/homelab/ansible/playbooks/setup-proxmoxve/trinity-helios/helios-setup.yml
+    fi
 
     echo "helios-setup bash script completed successfully!"
 else
