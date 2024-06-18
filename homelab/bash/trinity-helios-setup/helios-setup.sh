e25c9230f5ee8f147008f7cbfe9883f1ea1c9344
NayanTheSpaceGuy <147062173+NayanTheSpaceGuy@users.noreply.github.com>
2024-06-18 22:47:34 +0530

    Added more functions and reorganised the script

--- homelab/bash/trinity-helios-setup/helios-setup.sh
+++ homelab/bash/trinity-helios-setup/helios-setup.sh
@@ -9,7 +9,7 @@
 # Special Functions
 ###################
 # Detect Linux distribution
-distribution ()
+function distribution ()
 {
         local dtype="unknown"  # Default to unknown
 
@@ -45,7 +45,7 @@ distribution ()
 }
 
 # Detect locale
-locale ()
+function locale ()
 {
     local localetype="unknown" # Default to unknown
 
@@ -58,94 +58,168 @@ locale ()
     echo "$localetype"
 }
 
+############
+# Functions
+###########
+
+function header_info ()
+{
+    clear
+    echo " _   _  _____  _      _____  _____  _____            _____  _____  _____  _   _ ______  "
+    echo "| | | ||  ___|| |    |_   _||  _  |/  ___|          /  ___||  ___||_   _|| | | || ___ \ "
+    echo "| |_| || |__  | |      | |  | | | |\ '--.   ______  \ '--. | |__    | |  | | | || |_/ / "
+    echo "|  _  ||  __| | |      | |  | | | | '--. \ |______|  '--. \|  __|   | |  | | | ||  __/  "
+    echo "| | | || |___ | |____ _| |_ \ \_/ //\__/ /          /\__/ /| |___   | |  | |_| || |     "
+    echo "\_| |_/\____/ \_____/ \___/  \___/ \____/           \____/ \____/   \_/   \___/ \_|     "
+    echo ""
+    echo "Loading..."
+}
+
+function base_installation ()
+{
+    echo "Updating package lists and dist-upgrading packages..."
+    apt-get update
+    apt-get dist-upgrade -y
+
+    echo "Installing required packages (curl, sops, age, git, ansible)..."
+    apt-get install -y curl age git ansible
+
+    SOPS_LATEST_VERSION=$(curl -s "https://api.github.com/repos/getsops/sops/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+    curl -Lo sops.deb "https://github.com/getsops/sops/releases/download/v${SOPS_LATEST_VERSION}/sops_${SOPS_LATEST_VERSION}_amd64.deb"
+    apt --fix-broken install ./sops.deb
+
+    echo "Cleaning up..."
+    rm -rf sops.deb
+}
+
+github_deploy_key_setup ()
+{
+    echo "Setting up GitHub Deploy Key..."
+
+    # Check if ~/.ssh/deploy-dotfiles-and-homelab-ntsg exists
+    if [ -f ~/.ssh/dotfiles-and-homelab-ntsg ]; then
+        echo "The key file ~/.ssh/deploy-dotfiles-and-homelab-ntsg already exists."
+        read -r -p "Do you want to overwrite it? (y/n) " overwrite
+        if [ "$overwrite" == "y" ]; then
+            read -r -p "Enter the new value for the key: " new_deploy_key_value
+            echo "$new_deploy_key_value" > ~/.ssh/deploy-dotfiles-and-homelab-ntsg
+            echo "Deploy key file updated with the new value."
+        else
+            echo "Keeping the existing deploy key file."
+        fi
+    else
+        read -r -p "Enter the value for the key: " deploy_key_value
+        echo "$deploy_key_value" > ~/.ssh/deploy-dotfiles-and-homelab-ntsg
+        echo "Deploy key file created with the provided value."
+    fi
+
+    # Update the GitHub repository configuration in ~/.ssh/config
+    echo "Updating ~/.ssh/config with GitHub repository configuration..."
+    {
+        echo "Host github.com-dotfiles-and-homelab"
+        echo "    Hostname github.com"
+        echo "    IdentityFile=$HOME/.ssh/deploy-dotfiles-and-homelab-ntsg"
+    } > ~/.ssh/config
+    echo "GitHub repository configuration updated in ~/.ssh/config"
+
+    echo "Finished setting up GitHub Deploy Key"
+}
+
+sops_setup ()
+{
+    echo "Setting up SOPS..."
+
+    # Check and create ~/.sops directory if it doesn't exist
+    if [ ! -d ~/.sops ]; then
+        echo "Creating ~/.sops directory..."
+        mkdir ~/.sops
+    fi
+
+    # Check if ~/.sops/dotfiles-and-homelab-key.txt exists
+    if [ -f ~/.sops/dotfiles-and-homelab-key.txt ]; then
+        echo "The key file ~/.sops/dotfiles-and-homelab-key.txt already exists."
+        read -r -p "Do you want to overwrite it? (y/n) " overwrite
+        if [ "$overwrite" == "y" ]; then
+            read -r -p "Enter the new value for the key: " new_sops_key_value
+            echo "$new_sops_key_value" > ~/.sops/dotfiles-and-homelab-key.txt
+            echo "SOPS key file updated with the new value."
+        else
+            echo "Keeping the existing SOPS key file."
+        fi
+    else
+        read -r -p "Enter the value for the key: " sops_key_value
+        echo "$sops_key_value" > ~/.sops/dotfiles-and-homelab-key.txt
+        echo "SOPS key file created with the provided value."
+    fi
+
+    # Check if SOPS_AGE_KEY_FILE is set correctly
+    SOPS_AGE_KEY_FILE_NEW_VALUE="$HOME/.sops/dotfiles-and-homelab-key.txt"
+    if [ "${SOPS_AGE_KEY_FILE:-}" != "$SOPS_AGE_KEY_FILE_NEW_VALUE" ]; then
+        echo "Setting SOPS_AGE_KEY_FILE environment variable"
+        export SOPS_AGE_KEY_FILE="$SOPS_AGE_KEY_FILE_NEW_VALUE"
+        if grep -q "^export SOPS_AGE_KEY_FILE=" "$HOME/.bashrc"; then
+            sed -i "s|^export SOPS_AGE_KEY_FILE=.*|export SOPS_AGE_KEY_FILE=$SOPS_AGE_KEY_FILE_NEW_VALUE|" "$HOME/.bashrc"
+        else
+            echo "export SOPS_AGE_KEY_FILE=$SOPS_AGE_KEY_FILE_NEW_VALUE" >> "$HOME/.bashrc"
+        fi
+    else
+        echo "SOPS_AGE_KEY_FILE environment variable already set correctly"
+    fi
+
+    source ~/.bashrc
+
+    echo "Finished setting up SOPS"
+}
+
+sops_decryption ()
+{
+    sops --decrypt --age $(cat $SOPS_AGE_KEY_FILE |grep -oP "public key: \K(.*)") --encrypted-regex '^(data|stringData)$' --in-place ./secret.yaml
+}
+
+run_ansible_playbook ()
+{
+    echo "Proceeding with ansible for further setup..."
+    echo "Running the helios-setup ansible playbook..."
+    HELIOS_SETUP_ANSIBLE_DIR="$HOME/helios-setup/dotfiles-and-homelab/homelab/ansible"
+    if [ "$(locale)" == "UTF-8" ]; then
+        ansible-playbook \
+        -i "${HELIOS_SETUP_ANSIBLE_DIR}/inventory/spacehlship.yml" \
+        "${HELIOS_SETUP_ANSIBLE_DIR}/playbooks/setup-proxmoxve/trinity-helios/helios-setup.yml"
+    else
+        LANG=en_IN.UTF_8 ansible-playbook \
+        -i ~/helios-setup/dotfiles-and-homelab/homelab/ansible/inventory/spacehlship.yml \
+        ~/helios-setup/dotfiles-and-homelab/homelab/ansible/playbooks/setup-proxmoxve/trinity-helios/helios-setup.yml
+    fi
+}
+
 #######
 # Main
 ######
-main_helios_setup ()
-{
-    if [ "$(distribution)" == "debian" ]; then
-        echo "-------------"
-        echo "Here we go..."
-        echo "-------------"
-        echo ""
-        echo "Detected Debian distribution. Proceeding with the setup..."
+set -eEuo pipefail
+if [ "$(distribution)" == "debian" ]; then
+    header_info
+    echo "Detected Debian distribution. Proceeding with the setup..."
+    echo "-------------"
+    echo "Here we go..."
+    echo "-------------"
+    echo ""
 
-        echo "Updating package lists and dist-upgrading packages..."
-        apt-get update
-        apt-get dist-upgrade -y
+    base_installation
+    github_deploy_key_setup
+    sops_setup
 
-        echo "Installing required packages (curl, sops, age, git, ansible)..."
-        apt-get install -y curl age git ansible
+    echo "Creating new setup directory and navigating to it..."
+    mkdir ~/helios-setup
+    cd ~/helios-setup
 
-        SOPS_LATEST_VERSION=$(curl -s "https://api.github.com/repos/getsops/sops/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
-        curl -Lo sops.deb "https://github.com/getsops/sops/releases/download/v${SOPS_LATEST_VERSION}/sops_${SOPS_LATEST_VERSION}_amd64.deb"
-        apt --fix-broken install ./sops.deb
-        rm -rf sops.deb
+    echo "Cloning GitHub repository using SSH..."
+    git clone --recurse-submodules git@github.com-dotfiles-and-homelab:NayanTheSpaceGuy/dotfiles-and-homelab.git
+    sops_decryption
 
-        echo "Setting up SOPS..."
+    run_ansible_playbook
 
-        # Check and create ~/.sops directory if it doesn't exist
-        if [ ! -d ~/.sops ]; then
-            echo "Creating ~/.sops directory..."
-            mkdir ~/.sops
-        fi
-
-        # Check if ~/.sops/dotfiles-and-homelab-key.txt exists
-        if [ -f ~/.sops/dotfiles-and-homelab-key.txt ]; then
-            echo "The key file ~/.sops/dotfiles-and-homelab-key.txt already exists."
-            read -r -p "Do you want to overwrite it? (y/n) " overwrite
-            if [ "$overwrite" == "y" ]; then
-                read -r -p "Enter the new value for the key: " new_key
-                echo "$new_key" > ~/.sops/dotfiles-and-homelab-key.txt
-                echo "Key file updated with the new value."
-            else
-                echo "Keeping the existing key file."
-            fi
-        else
-            read -r -p "Enter the value for the key: " key_value
-            echo "$key_value" > ~/.sops/dotfiles-and-homelab-key.txt
-            echo "Key file created with the provided value."
-        fi
-
-        # Check if SOPS_AGE_KEY_FILE is set correctly
-        SOPS_AGE_KEY_FILE_NEW_VALUE="$HOME/.sops/dotfiles-and-homelab-key.txt"
-        if [ "${SOPS_AGE_KEY_FILE:-}" != "$SOPS_AGE_KEY_FILE_NEW_VALUE" ]; then
-            echo "Setting SOPS_AGE_KEY_FILE environment variable"
-            export SOPS_AGE_KEY_FILE="$SOPS_AGE_KEY_FILE_NEW_VALUE"
-            if grep -q "^export SOPS_AGE_KEY_FILE=" "$HOME/.bashrc"; then
-                sed -i "s|^export SOPS_AGE_KEY_FILE=.*|export SOPS_AGE_KEY_FILE=$SOPS_AGE_KEY_FILE_NEW_VALUE|" "$HOME/.bashrc"
-            else
-                echo "export SOPS_AGE_KEY_FILE=$SOPS_AGE_KEY_FILE_NEW_VALUE" >> "$HOME/.bashrc"
-            fi
-        else
-            echo "SOPS_AGE_KEY_FILE environment variable already set correctly"
-        fi
-
-        source ~/.bashrc
-
-        echo "Finished setting up SOPS"
-
-        echo "Creating new setup directory and navigating to it..."
-        mkdir ~/helios-setup
-        cd ~/helios-setup || return
-
-        echo "Cloning GitHub repository using SSH..."
-        git clone --recurse-submodules git@github.com:NayanTheSpaceGuy/dotfiles-and-homelab.git
-
-        echo "Running the helios-setup ansible playbook..."
-        HELIOS_SETUP_ANSIBLE_DIR="$HOME/helios-setup/dotfiles-and-homelab/homelab/ansible"
-        if [ "$(locale)" == "UTF-8" ]; then
-            ansible-playbook \
-                -i "${HELIOS_SETUP_ANSIBLE_DIR}/inventory/spacehlship.yml" \
-                "${HELIOS_SETUP_ANSIBLE_DIR}/playbooks/setup-proxmoxve/trinity-helios/helios-setup.yml"
-        else
-            LANG=en_IN.UTF_8 ansible-playbook \
-                -i ~/helios-setup/dotfiles-and-homelab/homelab/ansible/inventory/spacehlship.yml \
-                ~/helios-setup/dotfiles-and-homelab/homelab/ansible/playbooks/setup-proxmoxve/trinity-helios/helios-setup.yml
-        fi
-
-        echo "helios-setup bash script and ansible playbook completed successfully!"
-    else
-        echo "This script is only intended to run on Debian distributions currently."
-    fi
-}
+    echo "helios-setup bash script and ansible playbook completed successfully!"
+else
+    header_info
+    echo "This script is only intended to run on Debian distributions currently."
+fi
