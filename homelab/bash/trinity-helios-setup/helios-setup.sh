19a976e71d74c8793df5a94f9a04cbe4df1f6104
NayanTheSpaceGuy <147062173+NayanTheSpaceGuy@users.noreply.github.com>
2024-06-22 02:02:33 +0530

    Fixed sops.deb installation warnings

--- homelab/bash/trinity-helios-setup/helios-setup.sh
+++ homelab/bash/trinity-helios-setup/helios-setup.sh
@@ -92,7 +92,8 @@ function base_installation ()
 
     SOPS_LATEST_VERSION=$(curl -s "https://api.github.com/repos/getsops/sops/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
     curl -Lo sops.deb "https://github.com/getsops/sops/releases/download/v${SOPS_LATEST_VERSION}/sops_${SOPS_LATEST_VERSION}_amd64.deb"
-    apt --fix-broken install ./sops.deb
+    dpkg -i ./sops.deb
+    apt-get install -f # Install missing dependencies
 
     echo ""
     echo "--------------------------------------------------------"
