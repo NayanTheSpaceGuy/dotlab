845cb3854ca6ffa2144d7b58c62f5c1935a08ff2
NayanTheSpaceGuy <147062173+NayanTheSpaceGuy@users.noreply.github.com>
2024-06-30 23:50:55 +0530

    added new opentofu project

---
+++ homelab/opentofu/proxmoxve/trinity-helios/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.1-rc3"
+    }
+  }
+  backend "http" {}
+}
+
+variable "pve_api_url" {
+  type      = string
+  sensitive = true
+}
+
+variable "pve_user" {
+  type = string
+}
+
+variable "pve_password" {
+  type      = string
+  sensitive = true
+}
+
+provider "proxmox" {
+  pm_api_url      = var.pve_api_url
+  pm_user         = var.pve_user
+  pm_password     = var.pve_password
+  pm_tls_insecure = false
+}
